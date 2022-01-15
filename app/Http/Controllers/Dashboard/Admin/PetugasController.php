ab99b0a0775db70e98ef00d7dc2b9a7f8a9eb7be
diasdigital <78405123+diasdigital@users.noreply.github.com>
2022-01-15 11:49:48 +0700

    Menambah fitur edit untuk petugas
    Controller:
    - Mengisi method edit dan update pada PetugasController
    
    View:
    - Menambah view untuk edit data petugas
    
    Route:
    - Mengatur parameter yang ada di route resource petugas menjadi akun
    
    Penjelasan:
    Disini aku mengatur agar admin bisa mengedit data petugas.
    Field pada form edit terisi selain password, karena password sudah berbentuk hash, masa iya harus ditampilin.
    Jadi aku buat if aja, kalau admin mengisi field password, maka akan ada validasi dan hashing untuk password baru tersebut.
    Jika tidak, maka field password yang ada di database akan diabaikan.
    
    Hal lainnya, aku menggunakan model dengan nama Akun untuk tabel petugas ini
    Laravel berpikir kalo parameter untuk route petugas adalah petuga, karena itu bentuk singular dari petugas.
    Awalnya aku mau ganti ke akun semuanya, tapi kan yang ditampilin hanya petugas.
    Lalu aku mau ubah jadi pengurus, sama aja kan huruf akhirnya s, bakal jadi penguru.
    Jadi aku ubah parameter yang ada di route itu menjadi petugas
    Masalah lain timbul. Karena aku pakai model namanya Akun, bukan petugas.
    Aku ubah deh parameternya dari petugas jadi akun (lihat perubahan pada web.php)
    Berhasil, Laravel akhirnya membaca kalau parameter itu ditujukan untuk model Akun.
    Aku catat disini biar aku inget gimana itu bisa bekerja.
    
    Catatan:
    - Buat fitur hapus petugas

--- app/Http/Controllers/Dashboard/Admin/PetugasController.php
+++ app/Http/Controllers/Dashboard/Admin/PetugasController.php
@@ -44,12 +44,32 @@ class PetugasController extends Controller
 
     public function edit(Akun $akun)
     {
-        //
+        return view('dashboard.pages.petugas.edit',[
+            'petugas' => $akun,
+            'eskul' => Eskul::all()
+        ]);
     }
 
     public function update(Request $request, Akun $akun)
     {
-        //
+        $validatedData = $request->validate([
+            'nama_petugas' => 'required|max:255|alpha',
+            'username' => 'required|min:4|max:255|alpha_num',
+            'id_eskul' => 'required|numeric'
+        ]);
+
+        if ($request->password) {
+            $plainPassword = $request->validate([
+                'password' => 'required|min:8|max:255|alpha_num'
+            ]);
+
+            $validatedData['password'] = Hash::make($plainPassword['password']);
+        };
+
+        Akun::where('id_akun', $akun->id_akun)
+                ->update($validatedData);
+
+        return redirect('/dashboard/petugas')->with('berhasil', "Data petugas $akun->nama_petugas berhasil diubah");
     }
 
     public function destroy(Akun $akun)
---
+++ resources/views/dashboard/pages/petugas/edit.blade.php
@@ -0,0 +1,67 @@
+@extends('dashboard.layouts.main')
+
+@section('konten')
+    
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Tambah Data Petugas</h1>
+    </div>
+
+    <div class="col-lg-8">
+        <form method="POST" action="/dashboard/petugas/{{ $petugas->id_akun }}" class="mb-5">
+            @csrf
+            @method('put')
+
+            <div class="mb-3">
+                <label for="nama_petugas" class="form-label">Nama Petugas</label>
+                <input type="text" name="nama_petugas" class="form-control @error('nama_petugas') is-invalid @enderror" id="nama_petugas" required autofocus value="{{ old('nama_petugas', $petugas->nama_petugas) }}">
+                @error('nama_petugas')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" id="username" required value="{{ old('username', $petugas->username) }}">
+                @error('username')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div id="tipsUsername" class="form-text">
+                    Username harus berisi minimal 4 karakter, terdiri dari huruf dan angka
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="password" value="{{ old('password') }}">
+                @error('password')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div id="tipsPassword" class="form-text">
+                    Password harus berisi 8-20 karakter, terdiri dari huruf dan angka
+                </div>
+            </div>
+
+            <div class="mb-5 col-4">
+                <label for="eskul" class="form-label">Eskul</label>
+                <select class="form-select" name="id_eskul">
+                    @foreach ($eskul as $eks)
+                    @if (old('id_eskul', $petugas->id_eskul) == $eks->id_eskul)
+                        <option value="{{ $eks->id_eskul }}" selected>{{ $eks->nama_eskul }}</option>
+                    @else
+                        <option value="{{ $eks->id_eskul }}">{{ $eks->nama_eskul }}</option>
+                    @endif
+                    @endforeach
+                </select>
+            </div>
+                        
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </form>
+    </div>
+
+@endsection
