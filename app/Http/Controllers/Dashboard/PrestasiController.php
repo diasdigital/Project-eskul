5adbcb66d45856beb2c71c3195f0bace8d4d1845
diasdigital <78405123+diasdigital@users.noreply.github.com>
2022-01-18 11:12:52 +0700

    Menambah fitur create prestasi
    Controller:
    - Menambah method create dan update pada PrestasiController
    
    View:
    - Membuat file view untuk create prestasi
    
    ==========
    Catatan:
    - Berikan regex kepada rules controller-controller lain yang perlu

--- app/Http/Controllers/Dashboard/PrestasiController.php
+++ app/Http/Controllers/Dashboard/PrestasiController.php
@@ -23,12 +23,32 @@ class PrestasiController extends Controller
 
     public function create()
     {
-        //
+        return view('dashboard.pages.prestasi.create', [
+            'tb_eskul' => Eskul::all()
+        ]);
     }
 
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'nama_prestasi' => 'required|max:255|regex:/^[a-zA-Z0-9\s]+$/',
+            'bukti_foto' => 'required|image|file|max:1024',
+            'peringkat' => 'required|regex:/^[a-zA-Z0-9\s]+$/',
+            'tingkat' => 'required|alpha',
+            'tahun_prestasi' => 'required|numeric',
+        ]);
+
+        $validatedData['id_eskul'] = (auth()->user()->id_eskul)
+            ? auth()->user()->id_eskul
+            : $request->validate(['id_eskul' => 'required'])['id_eskul'];
+
+        if($request->file('bukti_foto')) {
+            $validatedData['bukti_foto'] = $request->file('bukti_foto')->store('foto/prestasi');
+        }
+
+        Prestasi::create($validatedData);
+
+        return redirect('/dashboard/prestasi')->with('berhasil', 'Data prestasi berhasil ditambah!');
     }
 
     public function edit(Prestasi $prestasi)
