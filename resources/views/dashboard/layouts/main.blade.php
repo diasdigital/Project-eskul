0298cb13927e0cd131e0641c167d9e211de01a8d
diasdigital <78405123+diasdigital@users.noreply.github.com>
2022-02-02 08:40:11 +0700

    Memasang template frontend
    - Memindahkan file css dan js kedalam folder assets agar bareng dengan template
    - Menggunakan file index untuk halaman utama
    
    Lanjut pasang halaman-halaman lain dari template

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -9,11 +9,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
     <!-- Custom styles for this template -->
-    <link href="/css/dashboard.css" rel="stylesheet">
+    <link href="/assets/css/dashboard.css" rel="stylesheet">
 
     {{-- Trix editor --}}
-    <link rel="stylesheet" type="text/css" href="/css/trix.css">
-    <script type="text/javascript" src="/js/trix.js"></script>
+    <link rel="stylesheet" type="text/css" href="/assets/css/trix.css">
+    <script type="text/javascript" src="/assets/js/trix.js"></script>
 
     <style>
       trix-toolbar [data-trix-button-group="file-tools"] {
@@ -65,6 +65,6 @@
     </script>
       
     
-    <script src="js/dashboard.js"></script>
+    <script src="/assets/js/dashboard.js"></script>
   </body>
 </html>
