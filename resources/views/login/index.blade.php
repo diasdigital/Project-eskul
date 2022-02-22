c838beee1a32e53dc5eda5cda06d354b05976761
diasdigital <78405123+diasdigital@users.noreply.github.com>
2022-02-22 10:53:41 +0700

    Mengubah halaman login, lagi

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -10,44 +10,33 @@
 
     <title>Hello, world!</title>
   </head>
-  <body style="background-color: #222f24">
-    
-    <div class="container col-xl-8 col-lg-10 col-md-12">
-      <div class="m-5 width-50 text-center bg-light bg-gradient" style="border-radius: 50px">
-        <div class="row align-items-center h-50">
-          <div class="col pt-5">
-            
-            <div class="row mb-5">
-              <div class="col">
-                <h2 class="heading-section">LOGIN</h2>
-              </div>
-            </div>
-            <div class="row justify-content-center">
-              <div class="col-lg-8 col-md-10">
+  <body>
+    <section class="vh-100" style="background-color: #508bfc;">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card shadow-2-strong" style="border-radius: 1rem;">
+              <div class="card-body p-5 text-center">
                 <form action="/login" method="POST">
                 @csrf
-                  <div class="mb-4">
-                    <input type="text" name="username" class="form-control @if(session()->has('loginError')) is-invalid @endif" id="username" placeholder="Username">
-                    <div class="invalid-feedback">
-                      {{ session('loginError') }}
-                    </div>
+                  <h3 class="mb-5">Login</h3>
+      
+                  <div class="form-outline mb-4">
+                    <input type="text" name="username" placeholder="Username" class="form-control form-control-lg" />
                   </div>
-                  <div class="mb-4">
-                    <input type="password" name="password" class="form-control" id="password" placeholder="Kata sandi">
+      
+                  <div class="form-outline mb-4">
+                    <input type="password" name="password" placeholder="password" id="typePasswordX-2" class="form-control form-control-lg" />
                   </div>
-                  <button class="w-50 mt-5 btn text-white fw-bold mb-3" style="background-color: rgb(33, 80, 37)" type="submit">Login</button>
+
+                  <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
                 </form>
               </div>
             </div>
           </div>
-          <div class="col">
-            <img src="/assets/img/login.jpg" class="img-fluid" style="border-top-right-radius: 50px; border-bottom-right-radius: 50px">
-          </div>
         </div>
-        <div></div>
       </div>
-    </div>
-    
+    </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
